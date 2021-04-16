fc44e02bb79d77d271c060284b9139a55349f9e3
ricardoifc <ricardoifc>
2021-04-16 11:25:12 -0500

    subir

--- run.py
+++ run.py
@@ -17,4 +17,4 @@ def suma():
     return "<h3>10 + 10 = %d</h3>" % (resultado)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
