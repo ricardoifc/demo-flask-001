1d39e41c45d4d8de73a7dc280753d768b21de2dc
ricardoifc <ricardoifc>
2021-04-16 12:04:54 -0500

    listadoPriviciasEcuador

--- run.py
+++ run.py
@@ -16,5 +16,11 @@ def suma():
     resultado = 10 + 10
     return "<h3>10 + 10 = %d</h3>" % (resultado)
 
+@app.route('/listado')
+def listado():
+    lista = ['prinvinciasEcuador','Azuay','Bolívar','Cañar','Carchi','Chimborazo','Cotopaxi','El Oro','Esmeraldas','Galápagos','Guayas','Imbabura','Loja','Los Ríos','Manabí','Morona Santiago','Napo','Orellana','Pastaza','Pichincha','Santa Elena','Santo Domingo de los Tsáchilas','Sucumbíos','Tungurahua','Zamora Chinchipe']
+    listado = ' <BR>'.join(lista)
+    return "<h1>%s</h1>" % (listado)
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
